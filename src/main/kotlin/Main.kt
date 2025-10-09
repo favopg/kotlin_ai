624466a62b179f17d1cb2c49d90c80652af04cdf
favopg <favoriteigo0904@gmail.com>
2025-10-09 21:02:32 +0900

    feat: ユーザー入力を処理する新機能を追加
    - `createInput()` 関数を追加し、ユーザーからの入力を受け付けて処理する機能を実装
    - 'exit' と入力することでアプリケーションを終了できるインタラクティブなループを導入
    - 入力内容をリアルタイムで標準出力に表示

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,4 +2,19 @@ package org.example
 
 fun main() {
     println("Hello, world!")
+    createInput()
 }
+
+fun createInput() {
+    while (true) {
+        print("Enter input (type 'exit' to quit): ")
+        val input = readln()
+        if (input.equals("exit", ignoreCase = true)) {
+            println("Exiting the application...")
+            break
+        } else {
+            println("You entered: $input")
+        }
+    }
+}
+
